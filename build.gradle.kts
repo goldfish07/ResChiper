c73700ac39c793337a6e155ec688357e6fb0d8b7
Ayush Bisht <ayushbisht5663@gmail.com>
2023-09-26 13:50:36 +0530

    Update dependencies: dom4j to 2.1.4 and guava to 32.0.1-jre
    Signed-off-by: Ayush Bisht <ayushbisht5663@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,11 @@ dependencies {
     implementation("org.jetbrains:annotations:24.0.0")
     implementation("com.android.tools.build:gradle:4.2.2")
     implementation("com.android.tools.build:bundletool:1.0.0")
-    implementation("com.google.guava:guava:30.0-jre")
+    implementation("com.google.guava:guava:32.0.1-jre")
     implementation("com.android.tools.build:aapt2-proto:0.4.0")
     implementation("commons-codec:commons-codec:1.16.0")
     implementation("commons-io:commons-io:2.13.0")
-    implementation("org.dom4j:dom4j:2.1.0")
+    implementation("org.dom4j:dom4j:2.1.4")
     implementation("com.google.auto.value:auto-value:1.5.4")
     annotationProcessor("com.google.auto.value:auto-value:1.5.4")
 }
