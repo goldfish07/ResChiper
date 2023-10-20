bb93824ea8c2fdd8b127931b77be9d3725c9f829
Ayush Bisht <ayushbisht5663@gmail.com>
2023-10-20 15:35:03 +0530

    [dependencies] Update grpc-protobuf: fix vulnerabilities [CVE-2022-3509], [CVE-2021-22569]
    [CVE-2022-3509] CWE-20: Improper Input Validation
    
    Description
    A parsing issue similar to CVE-2022-3171, but with textformat in protobuf-java core and lite versions prior to 3.21.7, 3.20.3, 3.19.6 and 3.16.3 can lead to a denial of service attack. Inputs containing multiple instances of non-repeated embedded messages with repeated or unknown fields causes objects to be converted back-n-forth between mutable and immutable forms, resulting in potentially long garbage collection pauses. We recommend updating to the versions mentioned above.
    
    [CVE-2021-22569] CWE-noinfo
    
    Description
    An issue in protobuf-java allowed the interleaving of com.google.protobuf.UnknownFieldSet fields in such a way that would be processed out of order. A small malicious payload can occupy the parser for several minutes by creating large numbers of short-lived objects that cause frequent, repeated pauses. We recommend upgrading libraries beyond the vulnerable versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("com.android.tools.build:gradle:7.0.0")
     implementation("com.android.tools.build:bundletool:1.6.0")
     implementation("com.google.guava:guava:32.0.1-jre")
-    implementation("io.grpc:grpc-protobuf:1.21.1")
+    implementation("io.grpc:grpc-protobuf:1.54.0")
     implementation("com.android.tools.build:aapt2-proto:7.0.0-7396180")
     implementation("commons-codec:commons-codec:1.16.0")
     implementation("commons-io:commons-io:2.13.0")
