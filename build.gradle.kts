378f5c4dd0d53bcede06516f676edf867c4b9855
Ayush Bisht <ayushbisht5663@gmail.com>
2023-12-07 20:43:28 +0530

    [dependencies][JUnit] upgrade from 5.9.1 to 5.10.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     implementation(gradleApi())
